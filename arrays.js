fb91d18374f272e7071a223771cb441998ebf6ba
William Step <william.step.0205@gmail.com>
2023-07-03 19:57:03 -0700

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -10,3 +10,12 @@ let scrimbaUserKey = scrimbaUsers["sindre@scrimba"] // DOES NOT WORK
 let scrimbaUsersEmails = Object.values(scrimbaUsers) // ["sindre@scrimba", "per@scrimba", "frode@scrimba"]
 let scrimbaUsersIDs = Object.keys(scrimbaUsers) // ["00", "01", "02"]
 let scrimbaUsersEntries = Object.entries(scrimbaUsers) // [["00", "sindre@scrimba"], ["01", "per@scrimba"], ["02", "frode@scrimba"]]
+
+const array = Object.values(scrimbaUsersEntries)
+array.forEach(function (data) { console.log(data) })
+// OR
+for (const data of array) { console.log(data) }
+// OR
+for (const [key, value] of Object.entries(scrimbaUsersEntries)) { console.log(`${key} - ${value}`) }
+// OR
+for (let i = 0; i < array.length; i++) { console.log(array[i]) }
